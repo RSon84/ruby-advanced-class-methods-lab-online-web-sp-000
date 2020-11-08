fe9cdaea8b964d5977559f228a36c74e7d45de6e
Devin Wilson <craigvin84@yahoo.com>
2020-11-08 02:09:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,35 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+new_song = self.new
+@@all << new_song
+new_song
+  end
+
+  def self.new_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    new_song
+end
+
+def self.create_by_name(song_name)
+  new_song = self.new
+  new_song.name = song_name
+  @@all << new_song
+  new_song
+end
+
+def self.find_by_name(song_name)
+self.all.detect { |x| x.name == song_name}
+end
+
+def self.find_or_create_by_name(song_name)
+self.find_by_name(song_name) ||
+    self.create_by_name(song_name)
+end
+
+def self.alphabetical
+  @@all.sort_by { |song| song.name }
+end
 end
