f45da90c8e6730fd9d594c3e608bc36af32a44ae
Devin Wilson <craigvin84@yahoo.com>
2020-11-08 02:20:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,3 +42,10 @@ def self.alphabetical
   @@all.sort_by { |song| song.name }
 end
 end
+
+def self.new_from_filename(name)
+  song = self.new 
+  song.name = (name.split(" - ")[1].chomp(".mp3"))
+  song.artist_name = (name.split(" - ")[0])
+  song
+end
